573fcd0185ab1d47dd3b78cbd438b467bbf5ee0e
natia <nati.shonia37@gmail.com>
2022-09-09 19:18:29 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Footer from "./component/Footer";
+import Home from './pages/Home';
+import Menu from './pages/Menu'
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import { Route, Routes } from "react-router-dom";
+
+// import { Menu } from "@mui/material";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+        <Navbar />
+     
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu/>} />
+     
+        <Route path="/about"element={<About/>} />
+        <Route path="/contact"  element={<Contact/>} />
+          </Routes>
+      <Footer />
+      
     </div>
   );
 }
